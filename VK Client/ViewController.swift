a54a5089b29d431fd86f1010cfe0e4b3393cd06b
DmitriiKornilov <FiskMac@gmail.com>
2021-12-28 12:26:39 +0500

    Initial Commit

---
+++ VK Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK Client
+//
+//  Created by Дмитрий Корнилов on 28.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
