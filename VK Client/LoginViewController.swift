a33d45632bdbef03ebb93cc7f1c3b01f2695055a
DmitriiKornilov <FiskMac@gmail.com>
2021-12-28 14:44:01 +0500

    LoginViewControllerWithKeyboard

--- VK Client/LoginViewController.swift
+++ VK Client/LoginViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTestField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidShow(_:)),
+                                               name: UIResponder.keyboardDidShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardDidHide),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+
+        let recognizer = UITapGestureRecognizer(target: self,
+                                                action: #selector(onTap))
+
+        recognizer.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(recognizer)
+    }
+
+    @objc func keyboardDidShow(_ notification: Notification) {
+        guard let keyboardHeight = ((notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)? .cgRectValue)?.height else {return}
+        let insets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+    }
+
+    @objc func onTap() {
+        self.view.endEditing(true)
+    }
+
+    @objc func keyboardDidHide() {
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 
     @IBAction func pressToLogin(_ sender: Any) {
         guard let login = loginTextField.text,
@@ -25,5 +63,4 @@ class LoginViewController: UIViewController {
             print("error")
         }
     }
-
 }
